d86c998a48d6cf2b097fa577f4b3cdbd873c644a
doerschera <doerschera@gmail.com>
2017-01-22 21:56:50 -0600

    alert markup

---
+++ app/main/components/ManagerAlert.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ManagerAlert extends React.Component {
+
+  render() {
+    <div className="manager-alert">
+      <h3>Alert Manager</h3>
+      <h5>Open order exceeds 4!</h5>
+      <button className"dont-button">OK</button>
+    </div>
+  }
+}
